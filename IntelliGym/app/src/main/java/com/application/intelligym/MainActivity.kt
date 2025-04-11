8d7a7b1e7200d6cb6d87341b1fcf433bff7af533
Daax06 <kokeshi.jm@gmail.com>
2025-04-11 16:58:29 +0800

    initiate project, created new files and began development

--- IntelliGym/app/src/main/java/com/application/intelligym/MainActivity.kt
+++ IntelliGym/app/src/main/java/com/application/intelligym/MainActivity.kt
@@ -3,45 +3,25 @@ package com.application.intelligym
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.application.intelligym.theme.IntelliGymTheme
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.FirebaseApp
+import com.application.intelligym.
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        FirebaseApp.initializeApp(this) // Initialize Firebase
+
         setContent {
             IntelliGymTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    IntelliGymApp() // Launch the full app
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntelliGymTheme {
-        Greeting("Android")
     }
 }
