9fa47b44c1bba8e9fea359ccab7cf584f9f63735
Daax06 <kokeshi.jm@gmail.com>
2025-04-12 23:33:04 +0800

    initiate project, created new files and began development

--- IntelliGym/app/src/main/java/com/application/intelligym/IntroActivity.kt
+++ IntelliGym/app/src/main/java/com/application/intelligym/IntroActivity.kt
@@ -5,20 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import com.application.intelligym.navigation.NavGraph
 import com.application.intelligym.theme.IntelliGymTheme
-import androidx.navigation.compose.rememberNavController
-import com.google.firebase.FirebaseApp
 
-class MainActivity : ComponentActivity() {
+class IntroActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this) // Initialize Firebase
-
         setContent {
             IntelliGymTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    com.application.intelligym.screens.LoginScreen() // Launch the full app
+                    NavGraph(
+                        navController = TODO()
+                    )
                 }
             }
         }
--- IntelliGym/app/src/main/java/com/application/intelligym/navigation/NavGraph.kt
+++ IntelliGym/app/src/main/java/com/application/intelligym/navigation/NavGraph.kt
@@ -4,25 +4,4 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.application.intelligym.screens.DashboardScreen
-import com.application.intelligym.screens.LoginScreen
-import com.application.intelligym.viewmodels.AuthViewModel
-import com.application.intelligym.screens.RegistrationScreen
 
-@Composable
-fun NavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "dashboard") {
-        composable("dashboard") {
-            DashboardScreen()
-        }
-    }
-}
-
-@Composable
-fun AppNavGraph(navController: NavHostController, viewModel: AuthViewModel) {
-    NavHost(navController, startDestination = "login") {
-        composable("login") { LoginScreen(navController, viewModel) }
-        composable("register") { RegistrationScreen(navController, viewModel) }
-        // Add other destinations here
-    }
-}
